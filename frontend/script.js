5c0f4579de47b54d9fcc9c31d6fcb246c3820517
Anshul shrivastava <vs092241@gmail.com>
2025-09-03 00:22:57 +0530

    frontend ready for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -38,7 +38,7 @@ form.addEventListener("submit", async (e) => {
   };
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/search", {
+    const response = await fetch("https://joblogy.onrender.com/search", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
